a7c4897375b812650291243456bb53baa5cd5159
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-07-01 15:12:30 +0800

    Fix the dplyr warning

--- R/panel_forest_plot.R
+++ R/panel_forest_plot.R
@@ -327,11 +327,7 @@ panel_forest_plot <- function(forest_data,
     mapped_data <- mapped_data %>%
       mutate_at(
         vars(x, xmin, xmax),
-        funs(if_else(
-          is.infinite(x) | is.infinite(xmin) | is.infinite(xmax),
-          NA_real_,
-          .
-        ))
+        ~if_else(is.infinite(.), NA_real_, .)
       )
   }
 
