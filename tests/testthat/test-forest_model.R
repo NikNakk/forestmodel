cf4449db9cff12bcd4df2d51f496d39f3bf57b78
Nick Kennedy <nick@nick-kennedy.com>
2020-07-20 16:50:26 +0100

    Updated to use fixed widths for panels

--- tests/testthat/test-forest_model.R
+++ tests/testthat/test-forest_model.R
@@ -8,12 +8,22 @@ test_that("forest_model function returns expected output", {
               `Meal Cal` = meal.cal
     )
 
+  mdl <- coxph(Surv(time, status) ~ ., pretty_lung)
+
+  panels <- default_forest_panels(mdl)
+
+  fixed_widths <- c(0.03, 0.099, 0.078, 0.041, 0.03, 0.41, 0.03, 0.19, 0.06, 0.03)
+  for (i in seq_along(fixed_widths)) {
+    panels[[i]]$width <- fixed_widths[i]
+  }
+
   fm <-
     forest_model(
-      coxph(Surv(time, status) ~ ., pretty_lung),
+      mdl,
       return_data = TRUE,
-      recalculate_width = 10,
-      recalculate_height = 7
+      panels = panels,
+      recalculate_width = FALSE,
+      recalculate_height = FALSE
     )
 
   vdiffr::expect_doppelganger("Sample forest_model plot for lung", fm$plot)
